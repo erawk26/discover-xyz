a5a4029e8001025abf81479e6aa7884c681cdd5a
Erik Olsen <erik.olsen@milespartnership.com>
2025-07-06 10:07:02 -0400

    refactor: streamline hostname handling in Next.js config

--- next.config.js
+++ next.config.js
@@ -5,17 +5,25 @@ import redirects from './redirects.js'
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
-
+const hostnames = [
+  NEXT_PUBLIC_SERVER_URL,
+  'http://localhost:3026',
+  'assets.simpleviewinc.com',
+  'media-cdn.tripadvisor.com',
+  'res.cloudinary.com',
+  '*.cloudfront.net',
+]
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
-        const url = new URL(item)
-
+      ...hostnames.map((item) => {
+        const url = new URL(item.startsWith('http') ? item : `https://${item}`)
         return {
           hostname: url.hostname,
           protocol: url.protocol.replace(':', ''),
+          port: url.port || '',
+          pathname: '/**',
         }
       }),
     ],
